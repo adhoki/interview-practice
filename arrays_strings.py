64cf9ff6a67bbd6de20a0450fdf5e228e2759d1f
adhokshaja Madhwaraj <adhokshaja.madhwaraj@nasdaq.com>
2021-09-06 15:04:21 -0400

    updated earlier hash table count, and added isStringPalindromePermutation

--- arrays_strings.py
+++ arrays_strings.py
@@ -36,14 +36,58 @@ def isStringPermutation(s1, s2):
         if char in s1_hash:
             s1_hash[char] += 1
         else:
-            s1_hash[char] = 0
+            s1_hash[char] = 1
     for char in s2:
         if char in s2_hash:
             s2_hash[char] += 1
         else:
-            s2_hash[char] = 0
+            s2_hash[char] = 1
     
     if s1_hash == s2_hash:
         return True
 
     return False
+
+def isStringPalindromePermutation(base_string, compare_string):
+    """
+    Algorithm:
+    - Check if the two strings are of same length, after removing non-alphanumeric characters
+    - create hashmap of both strings with chars as keys.
+    - if length is even, all counts should be even, and if odd, only one char can have odd count
+    """
+    base_string = base_string.replace(' ', '')
+    compare_string = compare_string.replace(' ', '')
+    
+    if len(base_string) != len(compare_string):
+        print('returning false in length compare')
+        return False
+
+    base_hash, compare_hash = {}, {}
+
+    for char in base_string:
+        if char in base_hash:
+            base_hash[char] += 1
+        else:
+            base_hash[char] = 1
+
+    for char in compare_string:
+        if char in compare_hash:
+            compare_hash[char] += 1
+        else:
+            compare_hash[char] = 1
+
+    # added values to both hash-tables 
+    # atmost 1 character can have odd values.
+    isOdd = 0
+    for val in compare_hash.values():
+        if val % 2 == 1:
+            isOdd += 1
+        if isOdd > 1:
+            return False
+
+    if compare_hash == base_hash:
+        return True
+
+    return False
+
+print(isStringPalindromePermutation('taco cat', 'atco cta'))
